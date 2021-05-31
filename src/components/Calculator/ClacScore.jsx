5e22fa93bf93f840863d5805f450c36418e75c4f
Chang Siang <siang07734@gmail.com>
2021-05-31 21:48:28 +0800

    Update ClacScore.jsx

--- src/components/Calculator/ClacScore.jsx
+++ src/components/Calculator/ClacScore.jsx
@@ -6,22 +6,22 @@ import Badge from 'react-bootstrap/Badge'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import Tooltip from 'react-bootstrap/Tooltip'
 
-const ClacScore = ({ mape, rmse }) => {
+const ClacScore = ({ mape, nrmse }) => {
     return (
         <div>
             <Row className="align-items-center rmse">
                 <Col xs={12} className="align-items-center score-type ">
-                        <Badge pill variant="danger">RMSE</Badge>
-                        <OverlayTrigger overlay={<Tooltip>{rmse}</Tooltip>} >
-                            <span className="score-value">{rmse.toFixed(4)}</span>
+                        <Badge pill variant="danger">nRMSE</Badge>
+                        <OverlayTrigger overlay={<Tooltip>{nrmse}</Tooltip>} >
+                            <span className="score-value">{nrmse.toFixed(2)}%</span>
                         </OverlayTrigger>
                 </Col>
             </Row>
             <Row className="align-items-center mape">
                 <Col xs={12} className="score-type">
-                    <Badge pill variant="primary">MAPE</Badge>
+                    <Badge pill variant="primary">&nbsp;MAPE&nbsp;</Badge>
                     <OverlayTrigger overlay={<Tooltip>{mape}</Tooltip>}>
-                        <span className="score-value">{mape.toFixed(4)}</span>
+                        <span className="score-value">{mape.toFixed(2)}%</span>
                     </OverlayTrigger>
                 </Col>
             </Row>
