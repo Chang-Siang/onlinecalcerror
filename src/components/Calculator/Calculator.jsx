3264ef9c28f108237b66fdfba3c73e576a1daf78
Chang Siang <siang07734@gmail.com>
2021-05-31 21:48:32 +0800

    Update Calculator.jsx

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -2,24 +2,20 @@ import React from 'react';
 import { Component } from 'react';
 import { readRemoteFile } from 'react-papaparse'
 import moment from 'moment';
-import { MAPE, RMSE } from '../../lib/Metrics'
+import { MAPE, nRMSE } from '../../lib/Metrics'
 // Bs components
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Badge from 'react-bootstrap/Badge'
+import Button from 'react-bootstrap/Button'
 
 // view components
-import DatePick from './DatePick'
 import ForecastReader from './ForecastReader'
 import ClacScore from './ClacScore'
-import CalcSaveResult from './CalcSaveResult'
 import '../../stylesheets/Calculator.scss';
 
 let ajaxUrl = "http://localhost:3020/"
 
-
-let defFrom = ""
-let defTo = ""
 
 class Calculator extends Component {
     // ---------------------------------------------------
@@ -27,45 +23,22 @@ class Calculator extends Component {
     // ---------------------------------------------------
     constructor() {
         super()
-        // this.handleDayClick = this.handleDayClick.bind(this);
         this.state = {
             isFileUpload: false,
-            originData: [],
-            username: "",
-            from: "",
-            to: "",
             mape: 0,
-            rmse: 0,
+            nrmse: 0,
+            real: [],
+            pred: []
         }
     }
 
     componentDidMount() {
-        console.log('componentDidMount()');
-        this.loadOriginData();
     }
 
     componentDidUpdate() {
-        console.log('componentDidUpdate()');
     }
 
     componentWillUnmount() {
-        console.log('componentWillUnmount()');
-    }
-
-    loadOriginData = () => {
-        readRemoteFile('https://raw.githubusercontent.com/Chang-Siang/onlinecalcerror/master/public/elec_data/elec_merge_20160101_20190930.csv', {
-            download: true,
-            header: true,
-            complete: (results) => {
-                defFrom = new Date(results.data[0].date)
-                defTo = new Date(results.data[(results.data).length - 2].date)
-                this.setState({
-                    originData: results.data,
-                    from: defFrom,
-                    to: defTo,
-                })
-            }
-        })
     }
 
     ajaxServerItemAdd = (addItem) => {
@@ -100,104 +73,88 @@ class Calculator extends Component {
     // ---------------------------------------------------
     // 主程式
     // ---------------------------------------------------
-    getOriginData = () => {
-        const newData = [...this.state.originData]
-
-        // time filte
+    getRealData = () => {
+        const newData = [...this.state.real]
         return newData
-            .filter((column) => (
-                this.state.from <= (new Date(column.date)) && (new Date(column.date)) <= this.state.to
-            ))
-            .map((column) => (
-                column.kW
-            ))
     }
 
-    forecastDataCleaning = (data) => {
+    getPredData = () => {
+        const newData = [...this.state.pred]
+        return newData
+    }
+
+    dataCleaning = (data) => {
         // 展開運算子, 壓平陣列
-        let newData = [].concat(...data)
-        isNaN(newData[0]) && newData.splice(0, 1)
-
-        // data clean
+        var newData = data.map(function (value) {
+            if (value === '') {
+                return '-'
+            } else if (Number.isNaN(Number(value))) {
+                return '-'
+            } else {
+                return Number(value)
+            }
+        })
+        console.log(newData)
         return newData
-            .filter((item) => (
-                item
-            ))
     }
 
-    dataCheck = (forecastData) => {
-        let originData = this.getOriginData()
-        // console.log('originData', originData)
-        // console.log('forecastData', forecastData)
+    calculateScore = (e) => {
 
-        if (originData.length !== forecastData.length) {
-            alert('上傳資料長度錯誤')
+        e.preventDefault()
+
+        const real = this.dataCleaning(this.getRealData())
+        const pred = this.dataCleaning(this.getPredData())
+
+        if (real.length !== pred.length) {
+            alert('資料長度不同')
             return
         }
 
-        this.calculateScore(originData, forecastData)
-    }
+        let newReal = [...real]
+        let newPred = [...pred]
 
-    calculateScore = (originData, forecastData) => {
-        let rmse = RMSE(originData, forecastData)
-        let mape = MAPE(originData, forecastData)
+        let i = 0;
+        while (i < newReal.length) {
+            if (newReal[i] === '-') {
+                newReal.splice(i, 1)
+                newPred.splice(i, 1)
+            } else if (newPred[i] === '-') {
+                newReal.splice(i, 1)
+                newPred.splice(i, 1)
+            } else {
+                i++
+            }
+        }
+
+        let nrmse = nRMSE(newReal, newPred)
+        let mape = MAPE(newReal, newPred)
 
         this.setState({
+            real: real,
+            pred: pred,
             isFileUpload: true,
-            rmse: rmse,
+            nrmse: nrmse,
             mape: mape,
         })
+    }
+
+    applyData = (target, data) => {
+        if (target === 'pred') {
+            this.setState({
+                pred: data,
+            })
+        } else {
+            this.setState({
+                real: data,
+            })
+        }
     }
 
     // ---------------------------------------------------
     // handle action
     // ---------------------------------------------------
-    handleFromChange = (from) => {
-        // 檢查是否為合法日期
-        const temp = new Date(from)
 
-        if (!isNaN(temp.valueOf())) {
-            from = new Date(from.getFullYear(), from.getMonth(), from.getDate(), 0, 0, 0)
-            if (defFrom > from) {
-                alert('Out of Range.')
-                from = defFrom;
-            }
-        }
-
-        this.setState({
-            from
-        })
-    }
-
-    handleToChange = (to) => {
-        // 檢查是否為合法日期
-        const temp = new Date(to)
-
-        if (!isNaN(temp.valueOf())) {
-            to = new Date(to.getFullYear(), to.getMonth(), to.getDate(), 0, 0, 0)
-            if (defTo < to) {
-                to = defTo;
-                alert('Out of Range.')
-            }
-        }
-
-        this.setState({
-            to
-        }
-        )
-    }
-
-    showFromMonth() {
-        const { from, to } = this.state;
-        if (!from) {
-            return;
-        }
-        if (moment(to).diff(moment(from), 'months') < 2) {
-            this.to.getDayPicker().showMonth(from);
-        }
-    }
-
-    handleForce = (data, fileName) => {
+    handleForce = (target, data, fileName) => {
         // 檢查附檔名，若不符合則判斷上傳失敗
         const validExts = [".xlsx", ".xls", ".csv"]
         const fileExt = fileName.substring(fileName.lastIndexOf('.'))
@@ -205,76 +162,89 @@ class Calculator extends Component {
             alert("檔案類型錯誤，可接受的副檔名有：" + validExts.toString())
             return
         }
+
         // 清洗 csv
-        let forecastData = this.forecastDataCleaning(data.data)
-        // 檢查長度
-        this.dataCheck(forecastData)
+        let newData = [].concat(...data.data)
+        // let ary = [].concat(...newData)
+        isNaN(newData[0]) && newData.splice(0, 1)
+
+        this.applyData(target, newData)
     }
 
-    handleChange = (event) => {
-        this.setState({ username: event.target.value });
-    }
+    handleTextareaChange = (target, e) => {
 
-    handleISaveResult = () => {
-        const saveItem = {
-            id: +new Date(),
-            name: this.state.username,
-            from: this.state.from,
-            to: this.state.to,
-            mape: this.state.mape,
-            rmse: this.state.rmse
-        }
-        if (this.state.isFileUpload) {
-            // 呼叫 Ajax存入資料庫
-            this.ajaxServerItemAdd(saveItem)
-            this.setState({ isFileUpload: false });
+        let ary = String(e.target.value).split('\n')
+        if (target === 'pred') {
+            this.setState({ pred: ary })
         } else {
-            alert("請重新上傳檔案")
+            this.setState({ real: ary })
         }
     }
 
     render() {
         return (
             <div className="Calculator">
-                <Row className="align-items-center calculator-date-range">
-                    <Col xs={12} md={4} className="row-title">
-                        <h2>Select a Date Range</h2>
+                <Row>
+                    <Col xs={12} md={6}>
+                        <Row className="align-items-center calculator-textarea">
+                            <Col xs={12} className="row-title">
+                                <h2>Real Answer</h2>
+                            </Col>
+                            <Col xs={12} className="row-textarea">
+                                <textarea value={this.state.real.join('\n')} placeholder='Please enter some numbers, separated by line breaks.' className="form-control" rows={10} onChange={(e) => this.handleTextareaChange('real', e)} />
+                                <br />
+                                <Badge pill variant="primary">
+                                    {this.state.real.length} Data Points
+                                </Badge>
+                            </Col>
+                            <Col xs={12} className="calculator-csv-reader">
+                                <ForecastReader handleForce={(...e) => this.handleForce('real', ...e)} />
+                                <Badge pill variant="secondary">
+                                    <a href='https://raw.githubusercontent.com/Chang-Siang/onlinecalcerror/master/public/elec_data/Sample(273Days).csv'>
+                                        <span className="glyphicon glyphicon-download-alt" aria-hidden="true"></span>
+                                        File format template
+                                    </a>
+                                </Badge>
+                            </Col>
+                        </Row>
                     </Col>
-                    <Col xs={12} md={8}>
-                        <DatePick
-                            from={this.state.from}
-                            to={this.state.to}
-                            handleFromChange={this.handleFromChange}
-                            handleToChange={this.handleToChange}
-                        />
-                        <Badge pill variant="primary">
-                            {(this.state.to - this.state.from) / (1000 * 3600 * 24) + 1} Days
-                        </Badge>
+                    <Col xs={12} md={6}>
+                        <Row className="align-items-center calculator-textarea">
+                            <Col xs={12} className="row-title">
+                                <h2>Predictive Value</h2>
+                            </Col>
+                            <Col xs={12} className="row-textarea">
+                                <textarea value={this.state.pred.join('\n')} placeholder='Please enter some numbers, separated by line breaks.' className="form-control" rows='10' onChange={(e) => this.handleTextareaChange('pred', e)} />
+                                <br />
+                                <Badge pill variant="primary">
+                                    {this.state.pred.length} Data Points
+                                </Badge>
+                            </Col>
+                            <Col xs={12} className="calculator-csv-reader">
+                                <ForecastReader handleForce={(...e) => this.handleForce('pred', ...e)} />
+                                <Badge pill variant="secondary">
+                                    <a href='https://raw.githubusercontent.com/Chang-Siang/onlinecalcerror/master/public/elec_data/Sample(273Days).csv'>
+                                        <span className="glyphicon glyphicon-download-alt" aria-hidden="true"></span>
+                                        File format template
+                                    </a>
+                                </Badge>
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
                 <hr />
-                <Row className="align-items-center calculator-csv-reader">
-                    <Col xs={12} md={4} className="row-title">
-                        <h2>Select CSV File</h2>
-                    </Col>
-                    <Col xs={12} md={8}>
-                        <ForecastReader handleForce={this.handleForce} />
-                        <Badge pill variant="secondary">
-                            <a href='https://raw.githubusercontent.com/Chang-Siang/onlinecalcerror/master/public/elec_data/Sample(273Days).csv'>
-                                <span className="glyphicon glyphicon-download-alt" aria-hidden="true"></span>
-                                File format template
-                            </a>
-                        </Badge>
+                <Row className="align-items-center">
+                    <Col xs={12} className="row-title">
+                        <Button variant="primary" onClick={this.calculateScore}>
+                            Calculate
+                        </Button>
+                        {/* <ClacScore rmse={this.state.rmse} mape={this.state.mape} /> */}
                     </Col>
                 </Row>
                 <hr />
                 <Row className="align-items-center calculator-score">
-                    <Col xs={12} md={4} className="row-title">
-                        {/* <ClacScore rmse={this.state.rmse} mape={this.state.mape} /> */}
-                    </Col>
-                    <Col xs={12} md={8}>
-                        <ClacScore rmse={this.state.rmse} mape={this.state.mape} />
-                        {/* <CalcSaveResult username={this.state.username} handleChange={this.handleChange} handleISaveResult={this.handleISaveResult} /> */}
+                    <Col xs={12}>
+                        <ClacScore nrmse={this.state.nrmse} mape={this.state.mape} />
                     </Col>
                 </Row>
             </div>
